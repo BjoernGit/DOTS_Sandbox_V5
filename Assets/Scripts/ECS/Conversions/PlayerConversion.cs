2672ecc9ad6c08071b89c5af558345f14cd07f08
björn glienke <bjoern.glienke@googlemail.com>
2020-05-02 15:09:59 +0200

    Copy of full local project

---
+++ Assets/Scripts/ECS/Conversions/PlayerConversion.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class PlayerConversion : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager manager, GameObjectConversionSystem conversionSystem)
+    {
+        AppManager.instance.playerEntity = entity;
+    }
+}
